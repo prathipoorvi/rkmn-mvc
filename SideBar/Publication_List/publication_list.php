25064d98089a1c2a06fc7cc349d30f9d7e8a04db
prathipoorvi <prathu14@gmail.com>
2016-08-13 16:38:42 +0530

    publication_list.xml file are added

---
+++ SideBar/Publication_List/publication_list.php
@@ -0,0 +1,22 @@
+<h1>Publication List</h1>
+<h2>&nbsp;</h2>
+<h3>Special Offer:</h3>
+<p>We are pleased to make available the entire set of Swami Vivekananda's Granthavali: Volume 1 - 10 (in Marathi) at a special discounted price of Rs.500. Offer valid till 31st December 2012.</p>
+<p>To order books please click here.</p>
+<h3>This is an electronic version of our entire Publication Catalogue.</h3>
+<h4>Important :</h4>
+<ul>
+	<li>No Credit Card is required to order our Publications.</li>
+	<li>All payments should be made through Draft or Money Order. These should be drawn in the name of "Ramakrishna Math, Nagpur".</li>
+	<li>Prices may be subject to change. All the price changes will be updated at earliest.</li>
+	<li>Shipping and Packing charges will be added later.</li>
+	<li>After we receive your order we will send you a Proforma Invoice with your order value plus the shipping and packing charges calculated against the weight of the parcel.</li>
+</ul>
+<h3>Jivan Vikas Magazine</h3>
+<ul>
+	<li>Annual Subscription: Rs.60/-</li>
+	<li>Life Membership: Rs.1,000/-</li>
+	<li>Annual Subscription (Foreign): Rs.1,050/-</li>
+</ul>
+<p>To order books please click here.</p>
+
