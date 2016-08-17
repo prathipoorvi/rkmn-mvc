6dd08b2e7f1325f27d028651481718e14cad8fc4
prathipoorvi <prathu14@gmail.com>
2016-08-17 15:03:16 +0530

    Current Events file are added

---
+++ SideBar/Current_Events/Festival_Calendar/festival_calendar.php
@@ -0,0 +1,95 @@
+<h1>Festival Calendar 2016-2017</h1>
+<h2>&nbsp;</h2>
+<h3>Year: 2016</h3>
+<P>Ekadashi Day 2015-2016</P>
+<p>Hanuman Jayanti</p>
+<p>22nd April | Friday</p>
+<p>Sri Shankaracharya Jayanti</p>
+<p>11th May | Wednesday</p>
+<p>Bhagavan Buddha Jayanti</p>
+<p>21st May | Saturday</p>
+<p>Phalaharini Kali Puja</p>
+<p>4th June | Saturday</p>
+<p><strong>Guru Purnima</strong></p>
+<p><strong>19th July | Tuesday</strong></p>
+<p>Swami Ramakrishnananda</p>
+<p>31st July | Sunday</p>
+<p>Swami Niranjanananda</p>
+<p>18th August | Thursday</p>
+<p>Janmashtami (Shyamnam)</p>
+<p>24th August | Wednesday</p>
+<p>Janmashtami (Pravachan)</p>
+<p>25th August | Thursday</p>
+<p>Swami Advaitananda</p>
+<p>31st August | Wednesday</p>
+<p>Sri Ganesh Chaturthi </p>
+<p>5th September | Monday</p>
+<p>Swami Abhedananda</p>
+<p>24th September | Saturday</p>
+<p>Swami Akhandananda</p>
+<p>30th September | Friday</p>
+<p><strong>Sri Durga Maha Ashtami</strong></p>
+<p><strong>9th October | Sunday</strong></p>
+<p>Sri Vijaya Dashami</p>
+<p>11th October | Tuesday</p>
+<p>Sri Sri Kali Puja</p>
+<p>29th October | Saturday</p>
+<p>Swami Subodhananda</p>
+<p>11th November | Friday</p>
+<p>Swami Vijnanananda</p>
+<p>13th November | Sunday</p>
+<p>Swami Premananda</p>
+<p>8th December | Thursday</p>
+<p><strong>Sri Sri Ma Sarada Devi</strong></p>
+<p><strong>20th December | Tuesday</strong></p>
+<p>Swami Shivananda</p>
+<p>24th December | Saturday</p>
+<p>Christmas Eve</p>
+<p>24th December | Saturday</p>
+<h3>Year: 2017</h3>
+<p>Swami Saradananda</p>
+<p>4th January | Wednesday</p>
+<p>Swami Turiyananda</p>
+<p>11th January | Wednesday</p>
+<p>National Youth Day </p>
+<p>12th January | Thursday</p>
+<p><strong>Swami Vivekananda</strong></p>
+<p><strong>19th January | Thursday</strong></p>
+<p>Swami Brahmananda (Ramnam Sankirtan)</p>
+<p>29th January | Sunday</p>
+<p>Swami Trigunatitananda</p>
+<p>31st January | Tuesday</p>
+<p>Sri Sri Saraswati Puja</p>
+<p>1st  February | Wednesday</p>
+<p>Swami Adbhutananda</p>
+<p>10th  February | Friday</p>
+<p>Maha Shiva Ratri</p>
+<p>24th February | Friday</p>
+<p><strong>Sri Sri Ramakrishna Deva</strong></p>
+<p><strong>28th February | Tuesday</strong></p>
+<p>Sri Chaitanya Dev (Holi)</p>
+<p>12th  March | Sunday</p>
+<p>Swami Yogananda</p>
+<p>16th March | Thursday</p>
+<p>Ram Navami</p>
+<p>5th April | Wednesday</p>
+<h3>Ekadashi Day 2016-2017</h3>
+<h4>2016</h4>
+<ul>
+	<li>4, 17 April</li>
+	<li>3, 17 May</li>
+	<li>1, 16 June</li>
+	<li>1, 15, 30 July</li>
+	<li>14, 28 August</li>
+	<li>13, 26 September</li>
+	<li>12, 26 October</li>
+	<li>11, 25 November</li>
+	<li>10, 24 December</li>
+</ul>
+<h4>2017</h4>
+<ul>
+	<li>9, 23 January</li>
+	<li>7, 22 February</li>
+	<li>8, 24 March</li>
+	<li>7 April</li>
+</ul>
